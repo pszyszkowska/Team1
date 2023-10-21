1e43d17b4f8b2ef7d17563f6bdcc3cba2d452273
kamil14512 <146453788+kamil14512@users.noreply.github.com>
2023-10-21 14:22:02 +0200

    Revert "marbolo"

--- program.py
+++ program.py
@@ -1,12 +1,21 @@
 from datetime import date
 
-wiek = int(input('Podaj swój wiek:'))
-plec = input('Podaj swoja plec: K/M')
-region = input("Czy jestes z regionu EUR czy z USA \n")
-if plec == 'M' and wiek >= 40 and region == 'USA':
-    print("Pierwsza paczka Marboro gratis!")
+birth_year = int(input('Podaj rok urodzenia:'))
+birth_month = int(input('Podaj miesiąc urodzenia:'))
+birth_day = int(input('Podaj dzień urodzenia:'))
+birth_date = date(birth_year,birth_month,birth_day)
+birthday = date(birth_year + 18,birth_month,birth_day)
+today_date = date.today()
+if today_date >= birthday:
+    print("Jesteś pełnoletni!")
 else:
-    print("OK")
+    print("Nie jesteś pełnoletni!")
+
+# Wybor regionu
+region = input("Czy jestes z regionu EUR czy z USA \n")
+
+    # zastosowanie petli ktora wy
+
 while not ((region == "EUR") or (region == "USA")):
     try:
         region = input("Kraj nie rozpoznany, Prosze wybrac jedna z opcji EUR albo USA \n")
