fb5ac4ea2a90ce0b1b87cf410322a2494cec58be
kamil14512 <kamil.boguszewski899@gmail.com>
2023-10-21 13:41:00 +0200

    aperol

--- program.py
+++ program.py
@@ -1,12 +1,6 @@
-from datetime import date
-
-birth_year = int(input('Podaj rok urodzenia:'))
-birth_month = int(input('Podaj miesiąc urodzenia:'))
-birth_day = int(input('Podaj dzień urodzenia:'))
-birth_date = date(birth_year,birth_month,birth_day)
-birthday = date(birth_year + 18,birth_month,birth_day)
-today_date = date.today()
-if today_date >= birthday:
-    print("Jesteś pełnoletni!")
+wiek = input("Podaj wiek: ")
+plec = input("Podaj płeć: ")
+if int(wiek) >= 30 and plec == "K":
+    print("Darmowy drink")
 else:
-    print("Nie jesteś pełnoletni!")
+    print("Zapłać za drinka")
