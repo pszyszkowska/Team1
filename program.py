d0523d861eee73100bbb5b9acec78b3e92ed20ba
pszyszkowska <szyszkowskapatrycja@gmail.com>
2023-10-21 13:44:04 +0200

    marbolo

--- program.py
+++ program.py
@@ -1,12 +1,9 @@
 from datetime import date
 
-birth_year = int(input('Podaj rok urodzenia:'))
-birth_month = int(input('Podaj miesiąc urodzenia:'))
-birth_day = int(input('Podaj dzień urodzenia:'))
-birth_date = date(birth_year,birth_month,birth_day)
-birthday = date(birth_year + 18,birth_month,birth_day)
-today_date = date.today()
-if today_date >= birthday:
-    print("Jesteś pełnoletni!")
+wiek = int(input('Podaj swój wiek:'))
+plec = input('Podaj swoja plec: K/M')
+region = 'USA'
+if plec == 'M' and wiek >= 40 and region == 'USA':
+    print("Pierwsza paczka Marboro gratis!")
 else:
-    print("Nie jesteś pełnoletni!")
+    print("OK")
