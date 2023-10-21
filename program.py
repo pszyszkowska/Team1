277c538660ab5acbb1f9fe915f59136f8fad2d4a
pszyszkowska <szyszkowskapatrycja@gmail.com>
2023-10-21 16:51:56 +0200

    Update

--- program.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-wiek = int(input('Podaj swój wiek:'))
-plec = input('Podaj swoja plec: K/M')
-region = 'USA'
-if plec == 'M' and wiek >= 40 and region == 'USA':
-    print("Pierwsza paczka Marboro gratis!")
-else:
-    print("OK")
